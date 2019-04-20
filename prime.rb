b08727258a9db4578a37402d20fa836f7f64070f
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 23:49:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,5 @@
 require "pry"
 
 def prime?(number)
-  range = (2..10000000)
-  range.each do |num|
-  if number > 1 && number % num != 0 
-    return true 
-  else 
-    return false
-  end
-end
+  if number < 1 || number 
 end 
